f3e32d0953646434eccb4b7e858f24ea291d1c66
IrakozeLoraine <mukezwa@gmail.com>
2024-09-27 23:02:51 +0400

    fix: add test for user

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -5,7 +5,7 @@ from models.user import User
 
 
 class test_User(test_basemodel):
-    """ """
+    """ Test for user"""
 
     def __init__(self, *args, **kwargs):
         """ """
@@ -16,19 +16,23 @@ class test_User(test_basemodel):
     def test_first_name(self):
         """ """
         new = self.value()
+        new.first_name = "Jacques"
         self.assertEqual(type(new.first_name), str)
 
     def test_last_name(self):
         """ """
         new = self.value()
+        new.last_name = "Beverly"
         self.assertEqual(type(new.last_name), str)
 
     def test_email(self):
         """ """
         new = self.value()
+        new.email = "l.irakoze2@alustudent.com"
         self.assertEqual(type(new.email), str)
 
     def test_password(self):
         """ """
         new = self.value()
+        new.password = "123jabelo"
         self.assertEqual(type(new.password), str)
