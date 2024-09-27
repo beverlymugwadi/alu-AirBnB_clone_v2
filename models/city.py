1a1a02503c2e663d76807b261d7a68a448fdcf46
IrakozeLoraine <mukezwa@gmail.com>
2024-09-28 00:28:32 +0400

    ref: revert back city and place

--- models/city.py
+++ models/city.py
@@ -1,25 +1,15 @@
 #!/usr/bin/python3
-"""
-    contains City class to represent a city
-"""
-
+""" City Module for HBNB project """
 from models.base_model import BaseModel, Base
-from models.state import State
-from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String, ForeignKey
-from os import environ
-
-storage_engine = environ.get("HBNB_TYPE_STORAGE")
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
-    """ City class :City class to represent a city """
+    """ The city class, contains state ID and name """
 
-    if (storage_engine == "db"):
-        __tablename__ = "cities"
-        state_id = Column(String(60), ForeignKey(State.id))
-        name = Column(String(128), nullable=False)
-        places = relationship("Place", backref="cities")
-    else:
-        name = ""
-        state_id = ""
+    __tablename__ = 'cities'
+
+    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+    name = Column(String(128), nullable=False)
+    places = relationship("Place", backref="cities")
