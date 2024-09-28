48e3d58b1a301d55ca9979da4a4f603006e7c9c7
IrakozeLoraine <mukezwa@gmail.com>
2024-09-28 17:32:46 +0400

    ref: storage file

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,12 @@
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy import create_engine
 from models.base_model import BaseModel, Base
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 class FileStorage:
 
