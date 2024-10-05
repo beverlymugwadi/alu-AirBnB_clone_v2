aed98aa56301c0380746862c5e32e2f2b2ea90e5
beverlymugwadi <b.mugwadi1@alustudent.com>
2024-10-05 09:26:47 +0000

    Compress before sending

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,25 +1,22 @@
 #!/usr/bin/python3
 """Fabric script which generates a tgz archive"""
-
 from datetime import datetime
-from fabric.api import local
 from os.path import isdir
+from fabric.api import local
 
 
 def do_pack():
     """
     Generates a tgz archive
-
     Returns:
         str: The file path of the archive
     """
-    
     try:
         date = datetime.now().strftime("%Y%m%d%H%M%S")
-        if isdir("versions") is False:
+        if not isdir("versions"):
             local("mkdir versions")
-        file_name = "versions/web_static_{}.tgz".format(date)
-        local("tar -cvzf {} web_static".format(file_name))
+        file_name = f"versions/web_static_{date}.tgz"
+        local(f"tar -cvzf {file_name} web_static")
         return file_name
-    except BaseException:
+    except Exception:
         return None
